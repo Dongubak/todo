5959a0f1ce4884329ca750bc835493fbe9e2f9d2
Dongubak <x001125@gmail.com>
2022-04-07 10:08:33 +0900

    몇가지 디자인 구현
    어제 꺼

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import './App.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import { Route, Link, Switch, Router } from 'react-router-dom'
+import { Route, Link, Switch, Router, useHistory, useParams } from 'react-router-dom'
+import data from './data.js';
+import profileImg from './기본프로필이미지.jfif';
+import { faBars, faPlus, faCircleDot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function App() {
+  let [todos, todosEdit] = React.useState(data); 
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -12,35 +17,90 @@ function App() {
           <Navbar.Brand href="#home">
             <img
               alt=""
-              src={ logo }
+              src={logo}
               width="30"
               height="30"
               className="d-inline-block align-top"
             />{' '}
-          React Bootstrap
+          React TodoList
           </Navbar.Brand>
         </Container>
       </Navbar>
       <Switch>
-        <Route path="/">
-          <Main></Main>
-        </Route>
         <Route path="/Upload">
-          
+          <Upload></Upload>
         </Route>
-        <Route path="/:detail">
-          
+        <Route path="/detail/:id">
+          <Detail></Detail>
+        </Route>
+        <Route path="/">
+          <Main todos={ todos }>
+          </Main>
         </Route>
       </Switch>
-      
     </div>
   );
 }
 
 function Main(props) {
+  let history = useHistory();
+  return(
+    <>
+      <div className="container_main">
+        <nav className="nav">
+          <FontAwesomeIcon icon={ faBars }/>
+          <FontAwesomeIcon icon={ faPlus } />
+        </nav>
+
+        <section className="profile_img">
+          <img src={ profileImg }>
+          </img>
+        </section>
+        <section className="profile_info">
+          <h2 style={ { margin: 0 } }>
+            KIM HYUK JOONG
+          </h2>
+          <p style={ { margin: 0 } }>
+            Dongubak.github
+          </p>
+        </section>
+          
+        <main className="todos">
+          {
+            props.todos.map( e => {
+              return(
+                <div className="element" onClick={ () => {
+                  history.push(`/detail/${ e.id }`)
+                } }>
+                  <div className="circle">
+                    <FontAwesomeIcon icon={ faCircleDot } />
+                  </div>
+                  <div className="content">
+                    { e.content }
+                  </div>
+                </div>
+              )
+            })
+          }
+        </main>
+      </div>
+    </>
+  )
+}
+
+function Upload(props) {
   return(
     <div>
-      메인페이지
+      업로드 페이지
+    </div>
+  )
+}
+
+function Detail(props) {
+  let { id } = useParams();
+  return(
+    <div className="detail">
+      
     </div>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 0,
+        content: '공부하기',
+        completed: false,
+    },
+    {
+        id: 1,
+        content: '밥먹기',
+        completed: false,
+    },
+    {
+        id: 2,
+        content: '내일하기',
+        completed: false,
+    }
+];
